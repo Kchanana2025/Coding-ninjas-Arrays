441be429b0ee42c9e71ff742b5fe96664c949a86
Kchanana2025 <kanikachanana2025@gmail.com>
2023-06-27 23:43:44 +0530

    delete_ith_node

--- Link List/deleteith.cpp
+++ Link List/deleteith.cpp
@@ -40,20 +40,38 @@ node *takeinput()
     }
     return head;
 }
-void deleteIthNode(node *head, int i)
+node *deleteIthNode(node *head, int i)
 { // i is basicallly index bro
     int count = 0;
-    if (head == NULL)
+    node *temp = head;
+    if (i == 0)
     {
-        return;
+
+        head = head->next;
+        delete temp;
+        return head;
     }
     while (temp != NULL && count < i - 1)
     {
         temp = temp->next;
         count++;
     }
-    if (temp->next != NULL)
+    if (temp->next != NULL) // this means that temp has reached the required node OR we can say that user has not entered i>length of link list-1
     {
+        node *a = temp->next;
+        node *b = a->next;
+        temp->next = b;
+        delete a; // we wanted to delete a (beech wala node) that is the reason we didnt write temp->next=temp->next->next
+    }
+    return head;
+}
+void print(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
     }
 }
 
@@ -63,6 +81,7 @@ int main()
     node *head = takeinput();
     cout << "ENTER THE VALUE OF i";
     cin >> i;
-    deleteIthNode(head, i);
+    head = deleteIthNode(head, i);
+    print(head);
     return 0;
 }
