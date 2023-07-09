e9999d39633c3cc197c323f2f215621ff3843316
Kchanana2025 <kanikachanana2025@gmail.com>
2023-07-09 16:37:58 +0530

    Create find_node.cpp

---
+++ Link List/find_node.cpp
@@ -0,0 +1,18 @@
+int findNode(Node *head, int n)
+{
+    Node *temp = head;
+    int counter = 0;
+    while (temp != NULL)
+    {
+        if (temp->data == n)
+        {
+            break;
+        }
+        counter++;
+        temp = temp->next;
+    }
+    if (temp == NULL || head == NULL)
+        return -1;
+    else
+        return counter;
+}
