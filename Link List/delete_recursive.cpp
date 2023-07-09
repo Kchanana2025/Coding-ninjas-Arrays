b282f143043cfd3497cf6e755fc01e52606800db
Kchanana2025 <kanikachanana2025@gmail.com>
2023-07-09 15:58:46 +0530

    Update delete_recursive.cpp

---
+++ Link List/delete_recursive.cpp
@@ -0,0 +1,15 @@
+Node *deleteNodeRec(Node *head, int i)
+{
+    if (head->next == NULL && i != 0) // mtlb ki aage badhte badhte last node pr aa gye lekin i ya toh negative no hai agee ka no hai therefore head jo tha ussi ko return krdo
+        return head;
+    if (i == 0)
+    {
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+        return head;
+    }
+    Node *temp = deleteNodeRec(head->next, i - 1);
+    head->next = temp;
+    return head;
+}
