216c1f2a9b4612bb83e80e99de1d04f03379900b
Kchanana2025 <kanikachanana2025@gmail.com>
2023-07-09 16:09:25 +0530

    Update insertion_recursive.cpp

--- Link List/insertion_recursive.cpp
+++ Link List/insertion_recursive.cpp
@@ -3,7 +3,8 @@ Node *InsertNode(Node *head, int i, Node *newnode)
     if (head == NULL && i != 0) // agr i length se bda daal dooge toh idhar ruk jaega
     // dhyan se socho iss condition ke baare mein apne head ka next bhja aur index
     //   km kr dia agr max to max last node bhi insert krni hui apko toh bhi finally i=0 aa jaega last
-    //   recursion call tak isliye ye contion lgai i>length-1 ko terminate krne waali wali
+    //   recursion call tak isliye ye contion lgai
+    // ye condition (i>length-1) ko terminate kr degi
     // aur user i<0 bhi daal skta hai toh wo bhi tackle ho gya(mtlb waise daalega nai)
     {
         return head;
