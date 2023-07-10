2204310740519f2f7a184e1f4d49265f8ee1c4a1
Kchanana2025 <kanikachanana2025@gmail.com>
2023-07-10 10:54:15 +0530

    Create append_N_to_end_Approach2.cpp

---
+++ Link List/append_N_to_end_Approach2.cpp
@@ -0,0 +1,29 @@
+Node *appendLastNToFirst(Node *head, int n)
+{
+    if (head == NULL || i == 0 || head->next == NULL || i > length(head) - 1)
+    {
+        return head;
+    }
+    Node *slow = head;
+    Node *fast = head;
+    for (int i = 0; i < n; i++)
+    { // jitni nodes peeche se chorni thi utni aage se chordi
+        fast = fast->next;
+    }
+    while (fast->next !=
+           NULL) // Dry run krke dekho ab aap fast ko utna aage badha rhe ho such
+                 // that link list end ho jaye aur slow bhi utna aage badhe toh
+                 // jab slow kochalane lge the tab bhi slow fast mein n nodee ka
+                 // difference that jab fast last mein pahuch gya tab bhi n nodes
+                 // ka difference hai toh fir finally slow uss nodes ki traf point
+                 // kr rha hai jiske next mein hamien null dalna hai
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+    Node *new_head = slow->next;
+    slow->next = NULL;
+    fast->next = head;
+    head = new_head;
+    return head;
+}
