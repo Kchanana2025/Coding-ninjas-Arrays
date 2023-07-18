1b86b3da6f8eda973c5c4fa65f4500184bf2b72f
Kchanana2025 <kanikachanana2025@gmail.com>
2023-07-18 23:11:02 +0530

    Create Binary_tree.h

---
+++ Binary_tree/Binary_tree.h
@@ -0,0 +1,20 @@
+#include <vector>
+template <typename T>
+class Binary_tree
+{
+public: // we made it public taki hamien getter aur setter functions na bnane padhe
+    T data;
+    Binary_tree<T> *left;
+    Binary_tree<T> *right;
+    Binary_tree(T data)
+    {
+        this->data = data;
+        left = NULL; // garbage set krna is not right,its better if you set it to NULL
+        right = NULL;
+    }
+    ~Binary_tree()
+    {                // if it is single node then on calling left delete NULL will be called and phir delete NULL ignore ho jata hai kuch ni krta delete NULL
+        delete left; // sbse pehle root ka(provided u have written delete(root)) left ka destructor call hoga aur phir uske left same hi right ke lie hoga aur poora delte will be deleted
+        delete right;
+    }
+};
