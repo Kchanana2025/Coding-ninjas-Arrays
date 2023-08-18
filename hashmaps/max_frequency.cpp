fa0a6491038d2580b66a319357029f5234e4d5a0
Kchanana2025 <kanikachanana2025@gmail.com>
2023-08-18 17:19:47 +0530

    Update max_frequency.cpp

--- hashmaps/max_frequency.cpp
+++ hashmaps/max_frequency.cpp
@@ -8,7 +8,8 @@ int highestFrequency(int arr[], int n)
 
     int maxFreq = INT_MIN;
     int element = 0;
-    for (int i = 0; i < output.size(); i++) // very good approach
+    for (int i = 0; i < output.size(); i++) // very good approach : hamien ye check krne ki zarurat naipadhi ki agar do elements ki frequency same hai toh jo phle array mein aaya hoga wo print hoga
+                                            // hmne seedha map se max print kr dia = wala condition hi check nai kia because jo array mein phla max aaya hoga wo map mein bhi  max  hoga.toh jab map mein mein iterate krenge toh sahi max hi ayega
     {
 
         if (maxFreq < output[arr[i]])
