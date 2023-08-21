4ddc8394d525278d77f3c378e1c305cbdb247fb5
Kchanana2025 <kanikachanana2025@gmail.com>
2023-08-21 20:40:54 +0530

    added hashing function

--- hashmaps/Hashmap_implementation.cpp
+++ hashmaps/Hashmap_implementation.cpp
@@ -2,6 +2,9 @@
 // abhi ke lie hm keys ko of type string choose kr rhe hai aur value ko toh template use kr hi skte hai
 // agar hamien generalize krna hai toh hamien hr type ke lie ek common hash code sochna padhega
 // badme hm aise generalize bhi kr skte hain ki in inbuilt classes ke lie jin  jin mein ye function hai unme ye hashcode use krlo
+#include <string>
+#include <iostream>
+using namespace std;
 template <typename V>
 class MapNode // node of link list
 {
@@ -25,7 +28,7 @@ template <typename V>
 class ourmap
 {
     MapNode<V> **buckets; // it is our array we declared it as double pointer as it is array of pointers i.e head of all link lists
-    int size;             // stores total size
+    int count;            // stores total size
     int numBuckets;       // stores no of buckets
 
 public:
@@ -52,12 +55,9 @@ public:
     {
         return count;
     }
-    V getvalue(string key) // return value corresponding to a particular key
-    {
-    }
 
 private:
-    int getbucketindex(string key)
+    int getBucketIndex(string key)
     {
         // calculation of hashcode
         int hashcode = 0;
@@ -72,11 +72,43 @@ private:
         // compression
         return hashcode % numBuckets;
     }
+    void rehash()
+    {
+        MapNode<V> **temp = buckets;
+        buckets = new MApNode<V> *[(2 * numBuckets)];
+        for (int i = 0; i < 2 * numBuckets; i++)
+        {
+            buckets[i] = NULL; // garbage addresses must not be present
+        }
+        int oldBucketCount = numBuckets;
+        numBuckets *= 2;
+        count = 0;
+        // we are taking ech node and putting it in our hashmap
+        for (int i = 0; i < oldBucketCount; i++)
+        {
+            MapNode<V> *head = temp[i];
+            while (head != NULL)
+            {
+                string key = head->key;
+                V value = head->value;
+                insert(key, value);
+                head = head->next;
+            }
+        }
+
+        // deleting of original hashmap
+        for (int i = 0; i < oldBucketCount; i++)
+        {
+            MapNode<V> *head = temp[i];
+            delete head;
+        }
+        delete[] temp;
+    }
 
 public:
     void insert(string key, V value) // inserts key value pair
     {
-        int bucketindex = getbucketindex(key);
+        int bucketIndex = getBucketIndex(key);
         MapNode<V> *head = buckets[bucketIndex];
         while (head != NULL)
         {
@@ -92,8 +124,13 @@ public:
         head = buckets[bucketIndex];
         MapNode<V> *node = new MapNode<V>(key, value);
         node->next = head; // we are inserting node in front of link list
-        bucket[bucketIndex] = node;
+        buckets[bucketIndex] = node;
         count++;
+        double loadFactor = (1.0 * count) / numBuckets; // int/int is integer so we multiplied it by 1.0 so that float/int gives float value
+        if (loadFactor > 0.7)
+        {
+            rehash();
+        }
     }
     V remove(string key) // delete key value pair(of the  key accepted as a parameter ) and return its value
     {
@@ -104,9 +141,9 @@ public:
         {
             if (head->key == key)
             {
-                if (prev == NULL) // this means that first element of link list contained the key in that case prev is NULL,ye wala case alag se isliye banana padha because NULL-> se segmentation fault ajata agar ye prev->next=head->next krte ,secondly bucketindex pr head bhi toh update karoge
+                if (prev == NULL) // this means that first element of link list contained the key in that case prev is NULL,ye wala case alag se isliye banana padha because NULL-> se segmentation fault ajata agar ye prev->next=head->next krte ,secondly bucketIndex pr head bhi toh update karoge
                 {
-                    buckets[bucketIndex] = head->next; // bucketindex will now contain new head
+                    buckets[bucketIndex] = head->next; // bucketIndex will now contain new head
                 }
                 else
                 {
@@ -124,7 +161,7 @@ public:
         return 0; // agar cantrol yaha pr aaya hai toh mtlb key present hi nai hogi toh return 0 krdo
                   // 0 kisi node ki value actually bhi ho skti thi pr filhal hmare pass option nai tha toh hmne 0 return kr dia
     }
-    V getValue(string key)
+    V getValue(string key) // return value corresponding to a particular key
     {
         int bucketIndex = getBucketIndex(key);
         MapNode<V> *head = buckets[bucketIndex];
@@ -137,5 +174,34 @@ public:
         }
         return 0;
     }
+    double getloadfactor()
+    {
+        double loadFactor = (1.0 * count) / numBuckets;
+        return loadFactor;
+    }
 };
 // array will contain  of heads of all link lists so its data type will be of double pointer
+int main()
+{
+    ourmap<int> map;
+    for (int i = 0; i < 10; i++)
+    {
+        char c = '0' + i;
+        string key = "abc";
+        key = key + c;
+        int value = i + 1;
+        map.insert(key, value);
+        cout << map.getloadfactor() << endl;
+    }
+    cout << map.size(); // 10
+    map.remove("abc2");
+    map.remove("abc7");
+    for (int i = 0; i < 10; i++)
+    {
+        char c = '0' + i;
+        string key = "abc";
+        key = key + c;
+        cout << key << ":" << map.getValue(key) << endl; // abc2,abc7 ke cooresponding value 0 aani chahie
+    }
+    cout << map.size(); // 8
+}
