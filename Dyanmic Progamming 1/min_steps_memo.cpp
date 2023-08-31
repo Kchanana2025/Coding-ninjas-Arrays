abd29b9ce51c005a29f5868cf842146d01005b99
Kchanana2025 <kanikachanana2025@gmail.com>
2023-08-31 19:12:11 +0530

    Update min_steps_memo.cpp

--- Dyanmic Progamming 1/min_steps_memo.cpp
+++ Dyanmic Progamming 1/min_steps_memo.cpp
@@ -1,8 +1,9 @@
 // O(n)
 // O(n)+O(n)(call stack and array)
+// memoization
 int countMinStepsToOne_helper(int n, int *output)
 {
-    int x, y = INT_MAX, z = INT_MAX;
+    int x = INT_MAX, y = INT_MAX, z = INT_MAX;
     //  base case
     if (n == 1)
     {
@@ -13,22 +14,27 @@ int countMinStepsToOne_helper(int n, int *output)
         return output[n];
     }
 
-    x = countMinStepsToOne(n - 1);
+    x = countMinStepsToOne_helper(n - 1, output);
 
     if (n % 2 == 0)
     {
-        y = countMinStepsToOne(n / 2);
+        y = countMinStepsToOne_helper(n / 2, output);
     }
     if (n % 3 == 0)
     {
-        z = countMinStepsToOne(n / 3);
+        z = countMinStepsToOne_helper(n / 3, output);
     }
     int ans = min(x, min(y, z));
     output[n] = ans + 1;
+    // cout<<output[n]<<" ";
     return output[n];
 }
 int countMinStepsToOne(int n)
 {
     int *output = new int[n + 1];
+    for (int i = 0; i <= n; i++) // hm apne output array ko kisi bhi value se initially set kr skte thee provided wo value answer ka part nai honi chahie eg -1 kabhi bhi no of steps ke barabar nai ho skta
+    {
+        output[i] = -1;
+    }
     return countMinStepsToOne_helper(n, output);
 }
